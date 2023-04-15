7b0f5b134c5b62b46549d206c798752f242f1e48
shelleyyyyy <shelleywr23@mail.vmi.edu>
2023-04-15 16:12:51 -0400

    fixed images

--- web/src/lib/app/SectorCard.svelte
+++ web/src/lib/app/SectorCard.svelte
@@ -1,17 +1,29 @@
 <script>
+	import { PUBLIC_POCKETBASE, PUBLIC_POCKETBASE_PORT } from '$env/static/public'
 	export let title;
 	export let description;
 	export let link;
 	export let img;
+	import { onMount } from 'svelte'
+	import Icon from '$lib/img/comingSoon.jpeg';
 
-	import Icon from '$lib/img/grit.png';
+	let imageSrc = `http://${PUBLIC_POCKETBASE}:${PUBLIC_POCKETBASE_PORT}/api/files/sectors/${link}/${img}`
+	
+	onMount(() => {
+		if(img === ""){
+			imageSrc = Icon
+			console.log("HITING IT")
+		}
+	})	
+
 </script>
 
-<div class="card lg:card-side bg-base-100 shadow-xl">
-	<figure><img src={Icon} alt={img} /></figure>
+<div class="card lg:card-side bg-base-300 shadow-xl">
+	<figure><img class="w-56 p-5" src={imageSrc} alt={img} /></figure>
+		
 	<div class="card-body">
 		<h2 class="card-title">{title}</h2>
-		<p>{description}</p>
+		<p class="w-56">{description}</p>
 		<div class="card-actions justify-end">
 			<a href={'/app/' + link} class="btn btn-primary">View</a>
 		</div>
--- web/src/routes/app/+page.svelte
+++ web/src/routes/app/+page.svelte
@@ -7,13 +7,14 @@
 	let records = [];
 	onMount(async () => {
 		records = await pb.collection('sectors').getFullList(200);
+		console.log(records)
 	});
 </script>
 
 <h1 class="font-bold text-xl px-10 py-2">Sectors</h1>
 
-<div class="grid grid-cols-3 gap-5 p-5">
+<div class="flex flex-wrap justify-center gap-5 p-5">
 	{#each records as d}
-		<SectorCard title={d.title} description={d.description} link={d.id} img={d.link} />
+		<SectorCard title={d.title} description={d.description} link={d.id} img={d.img} />
 	{/each}
 </div>
