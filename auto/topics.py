0e4b9f8338bf2809b212d125ee401a49a40a02db
BrentRip <btorregosa15@gmail.com>
2023-04-25 10:10:28 -0400

    Update topics.py

---
+++ auto/topics.py
@@ -0,0 +1,32 @@
+
+# broker info
+broker_address = '192.168.1.179'
+broker_port = 1883
+
+# Primary topics
+water_lvl_topic = 'primary/level'
+scraper_topic = 'primary/scraper'
+intake_topic = 'primary/pump'
+outtake_topic = 'outtake/pump'
+
+# Grit topics
+grit_pump_topic = 'grit/pump'
+grit_lvl_topic = 'grit/level'
+
+# Chlorine topics
+chlorine_ph_topic = 'chlorine/ph'
+chlorine_lvl_topic = 'chlorine/level'
+chlorine_pump_topic = 'chlorine/pump'
+base_pump_topic = 'basic/punp'
+
+# Dechlorine topics
+dechlorine_ph_topic = 'dechlorine/ph'
+dechlorine_lvl_topic = 'dechlorine/level'
+dechlorine_pump_topic = 'dechlorine/pump'
+acid_pump_topic = 'acid/pump'
+agitator_topic = 'quality/plug/agitator'
+
+# Quality topics
+quality_lvl_topic = 'quality/level'
+quality_tmp_topic = 'quality/temp'
+quality_pump_topic = 'quality/pump'
